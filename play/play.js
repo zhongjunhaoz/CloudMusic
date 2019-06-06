47d24daebcbfef640aac7565cf6d0c99ac2487d9
zhongjunhaoz <2236241424@qq.com>
2019-06-06 23:05:56 +0800

    update

--- play/play.js
+++ play/play.js
@@ -17,6 +17,7 @@ Page({
     // console.log(audioId,'1')
 
 
+    // 请求歌曲音频的地址，失败则播放出错，成功则传值给createBgAudio(后台播放管理器，让其后台播放)
     wx.request({
       url: API_BASE_URL + '/song/url',
       data: {
@@ -24,13 +25,28 @@ Page({
       },
       success: res => {
         console.log('歌曲url:',res)
-        if (res.statusCode === 200) {
-          this.createBgAudio(res.data.data[0]);  //把res.data.data[0]里的数据给createBgAudio
-        }
+        if (res.data.data[0].url === null ) {  //如果是MV 电台 广告 之类的就提示播放出错，并返回首页
+          console.log('播放出错')
+          wx.showModal({
+            content: '播放出错',
+            cancelColor:'#DE655C',
+            confirmColor: '#DE655C',
+            showCancel:false,
+            confirmText:'返回',
+            complete(){
+              wx.switchTab({
+                url: '/pages/index/index'
+              })
+            }
+          })
+        } else {
+          this.createBgAudio(res.data.data[0]);  //否则就播放歌曲
+          } //把res.data.data[0]里的数据给createBgAudio
       }
     })
 
-    // 获取歌曲详情
+
+    //获取到歌曲音频，则显示出歌曲的名字，歌手的信息，即获取歌曲详情；如果失败，则播放出错。
     wx.request({
       url: API_BASE_URL + '/song/detail',
       data: {
@@ -38,12 +54,27 @@ Page({
       },
       success: res => {
         console.log('歌曲详情', res);
-        if (res.statusCode === 200) {
+        if(res.data.songs.length === 0){
+          // console.log('无法获取到资源')
+          wx.showModal({
+            content: '播放出错',
+            cancelColor:'#DE655C',
+            confirmColor: '#DE655C',
+            showCancel:false,
+            confirmText:'返回',
+            complete(){
+              wx.switchTab({
+                url: '/pages/index/index'
+              })
+            }
+          })
+        } else{
           this.setData({
             song: res.data.songs[0]  //获取到歌曲的详细内容，传给song
           })
         }
-      }
+
+      },
     })
   },
 
@@ -52,8 +83,8 @@ Page({
     const bgAudioManage = wx.getBackgroundAudioManager(); //获取全局唯一的背景音频管理器。并把它给实例bgAudioManage
     app.globalData.bgAudioManage = bgAudioManage;         //把实例bgAudioManage(背景音频管理器) 给 全局
     bgAudioManage.title = 'title';                        //把title 音频标题 给实例
-    bgAudioManage.src = res.url;                          // res.url 在createBgAudio 为 照片路径
-    bgAudioManage.onPlay(res => {
+    bgAudioManage.src = res.url;                          // res.url 在createBgAudio 为 mp3音频  url为空，播放出错
+    bgAudioManage.onPlay(res => {                         // 监听背景音频播放事件
       this.setData({
         isPlay: true
       })
